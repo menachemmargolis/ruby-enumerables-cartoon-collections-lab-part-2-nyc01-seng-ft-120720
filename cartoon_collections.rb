da61025ce574aa1942c11d6c38b0dea3d63f70aa
menachem m margolis <margolismendel5728@gmail.com>
2020-12-01 20:37:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
     valid_calls.any?{|a|planeteer_calls.include?(a)}
 
-    
+
 
 end
